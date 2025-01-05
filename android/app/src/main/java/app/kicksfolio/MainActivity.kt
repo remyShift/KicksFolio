ac365e045192a72ac077fefe81ed64c350bdc340
Remy Cassagne <remy.engassac@icloud.com>
2025-01-05 22:56:49 +0100

    'done'

--- android/app/src/main/java/app/kicksfolio/MainActivity.kt
+++ android/app/src/main/java/app/kicksfolio/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.remyshift.KicksFolio
+package app.kicksfolio
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/app/kicksfolio/MainApplication.kt
+++ android/app/src/main/java/app/kicksfolio/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.remyshift.KicksFolio
+package app.kicksfolio
 
 import android.app.Application
 import android.content.res.Configuration
