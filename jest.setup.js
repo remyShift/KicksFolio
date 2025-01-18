38f41371e5df203942ebc1acd81098167645ce45
Remy Cassagne <remy.engassac@icloud.com>
2025-01-18 22:44:44 +0100

    seetup test

---
+++ jest.setup.js
@@ -0,0 +1,24 @@
+import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('expo-router', () => ({
+    router: {
+        replace: jest.fn(),
+        push: jest.fn(),
+    },
+    useRouter: () => ({
+        replace: jest.fn(),
+        push: jest.fn(),
+    }),
+}));
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+    require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
+
+jest.mock('expo-secure-store', () => ({
+    setItemAsync: jest.fn(),
+    getItemAsync: jest.fn(),
+    deleteItemAsync: jest.fn(),
+}));
+
+global.fetch = jest.fn(); 
