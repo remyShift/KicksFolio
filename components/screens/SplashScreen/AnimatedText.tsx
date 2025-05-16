dc88e09d1240e97db1047330dae8dccb460fb784
remyShift <remy.engassac@icloud.com>
2025-05-16 17:16:53 +0200

    fix splash screen animation

--- components/screens/SplashScreen/AnimatedText.tsx
+++ components/screens/SplashScreen/AnimatedText.tsx
@@ -1,5 +1,12 @@
-import { View } from 'react-native';
-import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
+import { View, Text } from 'react-native';
+import Animated, { 
+    useAnimatedStyle, 
+    withTiming, 
+    useSharedValue, 
+    withSpring,
+    FadeOut 
+} from 'react-native-reanimated';
+import { useEffect } from 'react';
 
 interface AnimatedLogoProps {
     text: string;
@@ -7,21 +14,35 @@ interface AnimatedLogoProps {
 }
 
 export const AnimatedLogo = ({ text, className = "text-white text-6xl font-bold font-actonia" }: AnimatedLogoProps) => {
-    const letters = text.split('');
+    const translateX = useSharedValue(100);
+    const opacity = useSharedValue(0);
+
+    const AnimatedView = Animated.createAnimatedComponent(View);
+    const AnimatedText = Animated.createAnimatedComponent(Text);
+
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateX: translateX.value }],
+            opacity: opacity.value
+        };
+    });
+
+    useEffect(() => {
+        translateX.value = withSpring(0, {
+            damping: 15,
+            stiffness: 100
+        });
+        opacity.value = withTiming(1, { duration: 1000 });
+    }, []);
 
     return (
-        <View className="flex-row">
-            {letters.map((letter, index) => (
-                <Animated.Text
-                key={index}
-                entering={FadeIn.duration(500).delay(index * 150)}
+        <AnimatedView className="flex-row" style={animatedStyle}>
+            <AnimatedText
                 exiting={FadeOut.duration(500)}
                 className={className}
-                style={{ marginRight: index < letters.length - 1 ? -12 : 0 }}
-                >
-                {letter}
-                </Animated.Text>
-            ))}
-        </View>
+            >
+                {text}
+            </AnimatedText>
+        </AnimatedView>
     );
 }; 
