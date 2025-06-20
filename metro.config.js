2b5c6b94b37bb49edb08c6fcbc182990f40419a4
remyShift <remy.engassac@icloud.com>
2025-06-21 00:27:16 +0200

    chore: update metro.config.js and package.json to remove quagga dependency

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,12 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
-const path = require("path");
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.alias = {
-    ...config.resolver.alias,
-    "@": path.resolve(__dirname, "./"),
+	...config.resolver.alias,
+	'@': path.resolve(__dirname, './'),
 };
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = withNativeWind(config, { input: './global.css' });
