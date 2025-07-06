fc79efe53441b190635b28221a259377d5dad204
remyShift <remy.engassac@icloud.com>
2025-07-06 17:39:02 +0200

    refactor: remove unused imports in editProfile test file for cleaner code

--- __tests__/screens/app/editProfile.test.tsx
+++ __tests__/screens/app/editProfile.test.tsx
@@ -1,7 +1,6 @@
 import EditProfileForm from "@/components/screens/app/settings/accountSettings/EditProfileForm";
-import { act, fireEvent, render, screen, waitFor } from "@testing-library/react-native";
-import { mockUseAuth, mockUser, mockUseSizeConversion } from "../auth/authSetup";
-import { ReactTestInstance } from "react-test-renderer";
+import { act, fireEvent, render, screen } from "@testing-library/react-native";
+import { mockUseAuth, mockUser } from "../auth/authSetup";
 import { fillAndBlurInput } from "@/__tests__/setup";
 
 describe('Edit Profile Form', () => {
